383842c905da7b389d24d648018fad181769e419
lovis-ambrose <lovisambrose212@gmail.com>
2023-11-23 17:39:45 +0300

    enhanced appearance

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ function pageTransitions(){
     })
 
     // toggle theme button,, may delete it
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click',() =>{
-        let element = document.body;
-        element.classList.toggle('light-mode')
-    })
+    // const themeBtn = document.querySelector('.theme-btn');
+    // themeBtn.addEventListener('click',() =>{
+    //     let element = document.body;
+    //     element.classList.toggle('light-mode')
+    // })
 }
 pageTransitions();
